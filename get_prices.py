23c730fe01493f11c7f41b2a05fa64033b97ee94
Benjamin Goff <bengoff@gmail.com>
2023-09-20 14:57:35 -0400

    initial commit

---
+++ get_prices.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite_connector
+
+db = sqlite_connector.sqlite_db("heating_oil.db")
+
+r = requests.get("https://www.maineoil.com/zone1.asp?type=0")
+soup = BeautifulSoup(r.text)
+
+table = soup.find_all("table")[0]
+for row in table.find_all("tr"):
+    row_data = [i for i in row.find_all("td")]
+    if row_data:
+        company, town, price, phone, date, calc = row_data
+        insert = [{
+            "company": company.text,
+            "town": town.text,
+            "price": price.text,
+            "phone": phone.text,
+            "price_date": date.text,
+            "calc": calc.text,
+        }]
+        print(insert)
+        db.insert_dict_list("prices", insert)
+
---
+++ table_creations.sql
@@ -0,0 +1,8 @@
+create table if not exists prices (
+    company TEXT,
+    town TEXT, 
+    price REAL, 
+    phone TEXT,
+    price_date TEXT,
+    calc TEXT
+    );
